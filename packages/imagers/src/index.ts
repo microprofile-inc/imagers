46dbf7754da013c2fe8ce02b63a2152f86415859
foxsir <838394225@qq.com>
2025-09-02 22:06:28 +0800

    feat: format code

--- packages/imagers/src/index.ts
+++ packages/imagers/src/index.ts
@@ -1,29 +1,29 @@
 import __wbg_init, {
-    crop,
-    blur,
-    invert,
-    resize as _resize,
-    rotate,
-    flip_horizontal,
-    flip_vertical,
-    brighten,
-    thumbnail
+  crop,
+  blur,
+  invert,
+  resize as _resize,
+  rotate,
+  flip_horizontal,
+  flip_vertical,
+  brighten,
+  thumbnail
 } from './libs/imagers';
 import {FilterType} from "./filter-type";
 
 function resize(bytes: Uint8Array, width: number, height: number, filter: FilterType) {
-    return _resize(bytes, width, height, filter);
+  return _resize(bytes, width, height, filter);
 }
 
 export {
-    __wbg_init as init,
-    crop,
-    blur,
-    invert,
-    resize,
-    rotate,
-    flip_horizontal,
-    flip_vertical,
-    brighten,
-    thumbnail
+  __wbg_init as init,
+  crop,
+  blur,
+  invert,
+  resize,
+  rotate,
+  flip_horizontal,
+  flip_vertical,
+  brighten,
+  thumbnail
 };
