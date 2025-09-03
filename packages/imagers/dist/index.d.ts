f78b68192a751f6dad4a026e79f276bf11651576
foxsir <838394225@qq.com>
2025-09-03 13:24:06 +0800

    feat: ignore dist folder

--- packages/imagers/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { default as ___wbg_init, crop, blur, invert, rotate, flip_horizontal, flip_vertical, brighten, thumbnail } from './libs/imagers';
-import { FilterType, ImageFormat } from './types';
-declare function resize(bytes: Uint8Array, width: number, height: number, filter: FilterType): Uint8Array<ArrayBufferLike>;
-declare function image_convert_format(bytes: Uint8Array, format: ImageFormat): Uint8Array<ArrayBufferLike>;
-export { __wbg_init as init, crop, blur, invert, resize, rotate, flip_horizontal, flip_vertical, brighten, thumbnail, image_convert_format };
