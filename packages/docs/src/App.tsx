dd6988acc1bc1fddee0170cffd0ce41f5fac545f
foxsir <838394225@qq.com>
2025-09-03 13:48:27 +0800

    feat: update wasm libs

--- packages/docs/src/App.tsx
+++ packages/docs/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import png from './bg.png';
 import {useEffect, useState} from "react";
 
-import { init, crop } from '@microprofile/imagers';
+import { init, fast_blur } from '@microprofile/imagers';
 await init();
 
 function App() {
@@ -10,16 +10,23 @@ function App() {
 
   useEffect(() => {
     fetch(png).then(res => res.bytes()).then(bytes => {
-      let result = crop(bytes, 1000, 1000, 100, 100);
+      let result = fast_blur(bytes, 200);
       const blob = new Blob([result], {type: 'image/png'});
       setSrc(URL.createObjectURL( blob ));
     })
   }, []);
   
   return (
-    <>
-      <img src={src} alt="" className={'w-56 h-56'} />
-    </>
+    <div className="bg-red-200 w-full">
+      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+        <h2 className="font-bold text-4xl">
+          Blur Image
+        </h2>
+        <div className="mt-10 flex items-center gap-x-6">
+          <img src={src} alt="" className={'w-fit'} />
+        </div>
+      </div>
+    </div>
   )
 }
 
