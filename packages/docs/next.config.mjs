825276350d620354c3c0a20a1bac12793e9680b5
foxsir <838394225@qq.com>
2025-09-05 18:41:01 +0800

    feat: update docs build

--- packages/docs/next.config.mjs
+++ packages/docs/next.config.mjs
@@ -15,6 +15,8 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   outputFileTracingIncludes: {
     '/**/*': ['./src/app/**/*.mdx'],
