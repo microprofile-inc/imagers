b993a2957399ff063a4cfabd9862009c62f11b50
foxsir <838394225@qq.com>
2025-09-06 09:55:08 +0800

    feat: set nextjs distDir

--- packages/docs/next.config.mjs
+++ packages/docs/next.config.mjs
@@ -15,6 +15,7 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: '../../dist',
   output: 'export',
   trailingSlash: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
